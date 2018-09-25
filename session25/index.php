57c67e1157fa2310a420762efae39ab83e0141ff
TranQuangHoai7292 <hoaitranquang7292@gmail.com>
2018-09-26 00:05:11 +0700

    webshop laravel

--- session25/index.php
+++
@@ -1,8 +0,0 @@
-<a href="index.php?action=home">HOME</a>
- | <a href="index.php?action=register">REGISTER</a>
- | <a href="index.php?action=LienHe">LienHe</a>
-<?php 
-	include 'controller/home_controller.php';
-	$home = new HomeController();
-	$home->handleRequest();
-?>
